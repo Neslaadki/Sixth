a4d7e2add9499d6ad42ac6b2310c0d6589ddc2d2
Dzyubak Anton <57284579+Neslaadki@users.noreply.github.com>
2022-02-13 13:07:27 +0300

    Update Main.java

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -9,18 +9,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        /**try {
-            Network network = new Network(8000);
-            Message message = (Message) network.read();
-            String commandName = message.getCommandName();
-            String arguments = message.getArgs();
-            City city = message.getCity();
-
-            network.write("Объект получен на сервере!");
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace(System.out);
-        }*/
+        
         ApplicationManager applicationManager = new ApplicationManager();
         applicationManager.initialisation();
     }
